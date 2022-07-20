a4c2a4b440398db34bba0674bd72e6ab178a80eb
Wen Xiao <wennstyle@gmail.com>
2022-07-19 23:07:10 -0400

    commit tree ab3816c99d284987527519d7a0b4d928c7930fe2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all # get all the games from the database
+    games.to_json # return a JSON response with an array of all the game data
   end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+
+  end
+
 
 end
